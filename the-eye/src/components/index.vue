4aa39e04b49e4b7b73766233c5e5f0c3c93e4b5c
Golden_CHJ <5605537+AM_CHJ0314@user.noreply.gitee.com>
2020-05-02 18:42:47 +0800

    classify_state

--- the-eye/src/components/index.vue
+++ the-eye/src/components/index.vue
@@ -16,14 +16,22 @@
 <div class="classify"><strong>{{cap}}</strong>{{classify}}</div>
 <div class="detail">{{detail}}</div>
 </div>
+
+<!-- <div class="info" v-show="isInfo">
+  {{info}}
+</div> -->
   <el-row class="wrapper1" id="button_wrapper">
     <input class="se2" id="f_file" type="file" name="image"  @change="predict($event)"/>
         <label for="f_file">
-            <input class="se1" type="button" value="识别" />
+            <input class="se1" type="button" :value="info" />
         </label>
 <!--
      <input type="file"  @change="predict($event)"/> -->
   </el-row>
+
+  <el-row   class="wrapper2" id="button_wrapper2" >
+  <el-button   class="wrapper3" id="back_button" type="danger" round disabled @click="back()">返回首页</el-button>
+</el-row>
 </div>
 
 </template>
@@ -44,6 +52,9 @@ export default {
   },
   data () {
     return {
+      isflash: false,
+      isInfo: false,
+      info: '识别',
       cap: '',
       classify: '这啥',
       detail: '可回收垃圾好啊',
@@ -55,45 +66,60 @@ export default {
     }
   },
   methods: {
+    back () {
+      alert(1)
+      this.$router.go(0)
+    },
     async  predict (e) {
+      document.getElementById('f_file').disabled = true
+      this.isInfo = true
       // const detail = document.getElementById('detailWrapper')
       const file = e.target.files[0]
+      this.info = '5% 获取图片成功'
       const imgDiv = document.getElementById('imgDiv')
       // const MODEL_PATH = 'http://127.0.0.1:8080'
       const BRAND_CLASSES = ['O', 'R']
       const mobilenet = await tf.loadLayersModel('http://localhost:8080/static/mobilenet/web_model/model.json')
-      mobilenet.summary()
+      this.info = '11% 载入mobilenet模型成功'
+      // mobilenet.summary()
       const layer = mobilenet.getLayer('conv_pw_13_relu')
+      this.info = '13% layer：conv_pw_13_relu'
       const truncatedMobilenet = tf.model({
         inputs: mobilenet.inputs,
         outputs: layer.output
       })
 
-      console.log(1)
+      this.info = '17% 截断层加载成功'
       const model = await tf.loadLayersModel('http://localhost:8080/static/mobilenet/web_model/model1.json')
-      console.log(2)
-
-      console.log(3)
-      console.log(file)
+      this.info = '45% 训练模型加载成功'
       const img = await file2img(file)
-      console.log(4)
+      this.info = '77% 图片处理成功'
+      while (imgDiv.hasChildNodes()) { // 当div下还存在子节点时 循环继续
+        imgDiv.removeChild(imgDiv.firstChild)
+      }
       const text = imgDiv.appendChild(img)
       text.style.width = '100%'
       text.style.hight = '100%'
       const pred = tf.tidy(() => {
-        console.log(5)
         const x = img2x(img)
         const input = truncatedMobilenet.predict(x)
         return model.predict(input)
       })
-      console.log(6)
+      this.info = '89% 预测功能tidy性能优化'
       const index = pred.argMax(1).dataSync()[0]
       setTimeout(() => {
-        alert(`预测结果：${BRAND_CLASSES[index]}`)
+        // alert(`预测结果：${BRAND_CLASSES[index]}`)
+        Velocity(document.getElementById('button_wrapper'), {opacity: 0}, {duration: 700})
       }, 0)
+      setTimeout(() => {
+        // alert(`预测结果：${BRAND_CLASSES[index]}`)
+        Velocity(document.getElementById('button_wrapper2'), {opacity: 1}, {duration: 1500})
+        document.getElementById('back_button').disabled = false
+      }, 750)
       let res = BRAND_CLASSES[index]
       this.hasShow = true
-      console.log(this.hasShow)
+      this.info = '100% 加载完成！'
+      this.isInfo = false
       if (res === 'R') {
         this.detail = '可回收物就是可以再生循环的垃圾。本身或材质可再利用的纸类、硬纸板、玻璃、塑料、金属、塑料包装，与这些材质有关的如：报纸、杂志、广告单及其它干净的纸类等皆可回收。'
         this.classify = 'ecyclable'
@@ -147,6 +173,17 @@ export default {
   width: 100%;
   top:50%;
 }
+.info{
+  /* background-color: #fff; */
+  color: white;
+  font-size: 1.2rem;
+   text-align: center;
+  font-weight: bold;
+  /* color: #000; */
+  position: absolute;
+  width: 100%;
+  top:75%;
+}
 .detailWrapper{
   color: #fff;
   position: absolute;
@@ -191,6 +228,14 @@ export default {
   right: 50%;
   width: 80%;
   margin: 0 auto;
+  text-align:center;
+}
+.wrapper2{
+  opacity: 0;
+  position: absolute;
+  top:90%;
+  margin: 0 auto;
+   width: 100%;
   text-align:center;
 }
 .se2{
